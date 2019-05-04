9fe9479f5cbc25a850e0302968f79a3620755ae6
Aleksey Belchenkov <u608110@gmail.com>
2019-05-04 16:20:19 +0300

    Processing Form Data

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.set('view engine', 'ejs');
+
+// Set Public Folder
 app.use(express.static('public'));
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body);
     res.render('index');
 });
 
